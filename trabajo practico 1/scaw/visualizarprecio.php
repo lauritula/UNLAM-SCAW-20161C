3c03848217dd2b5ad2b2af4b637320f451674a58
julianarabunal <julirabu@gmail.com>
2016-04-23 00:42:53 -0300

    mas.

--- trabajo practico 1/scaw/visualizarprecio.php
+++ trabajo practico 1/scaw/visualizarprecio.php
@@ -3,6 +3,10 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Visualización de precios</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
 <body>
 <div class="container">
@@ -42,9 +46,8 @@ if ($row = mysql_fetch_array($filtro_semana))
   		echo "¡ No se ha encontrado ningún registro !";
   	}
 ?>
+<br><input type="submit" class="btn btn-danger" value="Volver" onClick="location.href = 'indexUsuario.php' "></br>
 
-<br> <button type="button" class="btn btn-default">Default</button>
-<input type="submit"> value="Volver" onClick="location.href = 'indexUsuario.php' "></br>
 </div>
 </body>
 </html>
