9c21683c08ac13358e109e255288b6b1c05589d7
lauritula <mlaura.tula@gmail.com>
2016-05-16 13:52:06 -0300

    Revert "actualizacion de DB"
    This reverts commit 5d1141b59e204d1555f9aa73dcd310fa49c70402.

--- precioscuidados.sql
+++ precioscuidados.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-05-2016 a las 03:30:06
+-- Tiempo de generación: 16-05-2016 a las 01:07:33
 -- Versión del servidor: 10.1.13-MariaDB
 -- Versión de PHP: 5.6.20
 
@@ -27,7 +27,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `administrador` (
-  `id_admin` int(2) NOT NULL,
+  `id_admin` int(11) NOT NULL,
   `nombre` varchar(15) NOT NULL,
   `apellido` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -37,7 +37,29 @@ CREATE TABLE `administrador` (
 --
 
 INSERT INTO `administrador` (`id_admin`, `nombre`, `apellido`) VALUES
-(1, 'Diego', 'Reinoso');
+(1, 'John', 'Doe');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentarios`
+--
+
+CREATE TABLE `comentarios` (
+  `id_comentario` int(11) NOT NULL,
+  `id_comentarista` int(11) NOT NULL,
+  `semana` int(11) NOT NULL,
+  `texto` varchar(100) NOT NULL,
+  `fecha_hora` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `comentarios`
+--
+
+INSERT INTO `comentarios` (`id_comentario`, `id_comentarista`, `semana`, `texto`, `fecha_hora`) VALUES
+(1, 0, 16, 'Supermercado X negó el acceso', '2016-05-15 22:19:27'),
+(2, 2, 16, 'Ausente por enfermedad', '2016-05-15 22:41:19');
 
 -- --------------------------------------------------------
 
@@ -46,7 +68,7 @@ INSERT INTO `administrador` (`id_admin`, `nombre`, `apellido`) VALUES
 --
 
 CREATE TABLE `empleado` (
-  `id_empleado` int(2) NOT NULL,
+  `id_empleado` int(11) NOT NULL,
   `nombre` varchar(15) NOT NULL,
   `apellido` varchar(15) NOT NULL,
   `estado` varchar(10) NOT NULL
@@ -57,13 +79,8 @@ CREATE TABLE `empleado` (
 --
 
 INSERT INTO `empleado` (`id_empleado`, `nombre`, `apellido`, `estado`) VALUES
-(2, 'Julio', 'Reinoso', 'efectivo'),
-(3, 'Elizabeth', 'Perez', 'efectivo'),
-(4, 'Lucia', 'Reinoso', 'efectivo'),
-(5, 'Miriam', 'Reinoso', 'efectivo'),
-(7, 'Pedro', 'Picapiedra', 'efectivo'),
-(8, 'Juan', 'Peron', 'efectivo'),
-(9, 'Tony', 'Stark', 'efectivo');
+(0, 'Anonimo', 'Anonimo', 'efectivo'),
+(2, 'Cristian', 'Perez', 'efectivo');
 
 -- --------------------------------------------------------
 
@@ -72,12 +89,49 @@ INSERT INTO `empleado` (`id_empleado`, `nombre`, `apellido`, `estado`) VALUES
 --
 
 CREATE TABLE `precios` (
-  `id_producto` int(11) NOT NULL,
-  `id_empleado` int(11) NOT NULL,
-  `id_Entrada` int(11) NOT NULL,
+  `id_entrada` int(11) NOT NULL,
+  `id_producto` int(11) DEFAULT NULL,
+  `id_empleado` int(11) DEFAULT NULL,
   `descripcion` varchar(20) NOT NULL,
-  `semana` varchar(2) NOT NULL
+  `valor` int(11) NOT NULL,
+  `semana` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `precios`
+--
+
+INSERT INTO `precios` (`id_entrada`, `id_producto`, `id_empleado`, `descripcion`, `valor`, `semana`) VALUES
+(1, 1, NULL, 'Arroz', 100, 16),
+(2, 1, NULL, 'Arroz', 12, 17),
+(4, 2, NULL, 'Fideos', 30, 16),
+(5, 3, NULL, 'Yerba', 15, 16),
+(6, 4, NULL, 'Azucar', 4, 16),
+(7, 5, NULL, 'Salsa', 15, 16),
+(8, 2, NULL, 'Fideos', 28, 17),
+(9, 3, NULL, 'Yerba', 17, 17),
+(10, 4, NULL, 'Azucar', 8, 17),
+(11, 5, NULL, 'Salsa', 18, 17),
+(12, 1, NULL, 'Arroz', 15, 16),
+(13, 2, NULL, 'Fideos', 19, 16),
+(14, 3, NULL, 'Yerba', 55, 16),
+(15, 4, NULL, 'Azucar', 6, 16),
+(16, 5, NULL, 'Salsa', 11, 16),
+(17, 1, NULL, 'Arroz', 13, 17),
+(18, 2, NULL, 'Fideos', 21, 17),
+(19, 3, NULL, 'Yerba', 45, 17),
+(20, 4, NULL, 'Azucar', 9, 17),
+(21, 5, NULL, 'Salsa', 17, 17),
+(22, 1, NULL, 'Arroz', 16, 16),
+(23, 2, NULL, 'Fideos', 17, 16),
+(24, 3, NULL, 'Yerba', 53, 16),
+(25, 4, NULL, 'Azucar', 7, 16),
+(26, 5, NULL, 'Salsa', 16, 16),
+(27, 1, NULL, 'Arroz', 11, 17),
+(28, 2, NULL, 'Fideos', 18, 17),
+(29, 3, NULL, 'Yerba', 43, 17),
+(30, 4, NULL, 'Azucar', 6, 17),
+(31, 5, NULL, 'Salsa', 2, 17);
 
 -- --------------------------------------------------------
 
@@ -86,7 +140,7 @@ CREATE TABLE `precios` (
 --
 
 CREATE TABLE `producto` (
-  `id_producto` int(3) NOT NULL,
+  `id_producto` int(11) NOT NULL,
   `descripcion` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -95,12 +149,11 @@ CREATE TABLE `producto` (
 --
 
 INSERT INTO `producto` (`id_producto`, `descripcion`) VALUES
-(1, 'fideos'),
-(2, 'galletitas'),
-(3, 'desodorante'),
-(4, 'fernet'),
-(6, 'caramelos'),
-(7, 'pan rallado ');
+(1, 'Arroz'),
+(2, 'Fideos'),
+(3, 'Yerba'),
+(4, 'Azucar'),
+(5, 'Salsa de Tomate');
 
 -- --------------------------------------------------------
 
@@ -109,9 +162,9 @@ INSERT INTO `producto` (`id_producto`, `descripcion`) VALUES
 --
 
 CREATE TABLE `usuario` (
-  `id` int(2) NOT NULL,
+  `id` int(11) NOT NULL,
   `rol` varchar(15) NOT NULL,
-  `contrasenia` varchar(150) NOT NULL
+  `contrasenia` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -119,14 +172,9 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `rol`, `contrasenia`) VALUES
-(1, 'administrador', '$2y$12$2XF2zWvzfh3tB/MF9y0RPuS3CiwvID6F5ZKc0PpQk11d2hPzxApmu'),
-(2, 'empleado', '$2y$12$XXmCeXW5XBraTeWVoTElg.DkeuRtb3kH.Agu1s5Ao/Bp8N5/IDYp2'),
-(3, 'empleado', '$2y$12$FyxhV2g08m6tLPumr1Bmte0FORSGOISQfMdIfFwTBCMwBQIhev9iu'),
-(4, 'empleado', '$2y$12$ZEJuhfmOaxroLnyn.SvZouo2MXSjTeJwsYljq6xnHOLPrz01LNP8G'),
-(5, 'empleado', '$2y$12$g5se.0HTq2wG8.VInalzA.sf6/Gx2FpLEVmomqxssTlwNzrrr5MCy'),
-(7, 'empleado', '$2y$12$B5BkClAjSFkE6YLIdFevVuzJ4czuMbVtn46ldQ5XKXK5sni1LJMVq'),
-(8, 'empleado', '$2y$12$0NSSdoTFKVlPBMjn8swsRe5fDUdiq/Z45HnKYmXeRFxlkOQ.4tJNu'),
-(9, 'empleado', '$2y$12$IDWu1dgzcUOFsleKDCCRb.7sZNEw4jupXaRUfZ1qSGtFojrL0UeyW');
+(0, 'empleado', 'anonimo'),
+(1, 'administrador', 'uno'),
+(2, 'empleado', 'dos');
 
 --
 -- Índices para tablas volcadas
@@ -139,6 +187,13 @@ ALTER TABLE `administrador`
   ADD PRIMARY KEY (`id_admin`);
 
 --
+-- Indices de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`id_comentario`),
+  ADD KEY `id_comentarista` (`id_comentarista`);
+
+--
 -- Indices de la tabla `empleado`
 --
 ALTER TABLE `empleado`
@@ -148,8 +203,11 @@ ALTER TABLE `empleado`
 -- Indices de la tabla `precios`
 --
 ALTER TABLE `precios`
-  ADD PRIMARY KEY (`id_producto`,`id_empleado`),
-  ADD KEY `id_empleado` (`id_empleado`);
+  ADD PRIMARY KEY (`id_entrada`),
+  ADD KEY `id_producto` (`id_producto`),
+  ADD KEY `id_empleado` (`id_empleado`),
+  ADD KEY `id_producto_2` (`id_producto`),
+  ADD KEY `id_empleado_2` (`id_empleado`);
 
 --
 -- Indices de la tabla `producto`
@@ -168,15 +226,30 @@ ALTER TABLE `usuario`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `id_comentario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  MODIFY `id_empleado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT de la tabla `precios`
+--
+ALTER TABLE `precios`
+  MODIFY `id_entrada` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+--
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `id_producto` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- Restricciones para tablas volcadas
 --
@@ -185,20 +258,20 @@ ALTER TABLE `usuario`
 -- Filtros para la tabla `administrador`
 --
 ALTER TABLE `administrador`
-  ADD CONSTRAINT `administrador_ibfk_1` FOREIGN KEY (`id_admin`) REFERENCES `usuario` (`id`);
+  ADD CONSTRAINT `administrador_ibfk_1` FOREIGN KEY (`id_admin`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `empleado`
 --
 ALTER TABLE `empleado`
-  ADD CONSTRAINT `empleado_ibfk_1` FOREIGN KEY (`id_empleado`) REFERENCES `usuario` (`id`);
+  ADD CONSTRAINT `empleado_ibfk_1` FOREIGN KEY (`id_empleado`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `precios`
 --
 ALTER TABLE `precios`
-  ADD CONSTRAINT `precios_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`),
-  ADD CONSTRAINT `precios_ibfk_3` FOREIGN KEY (`id_empleado`) REFERENCES `empleado` (`id_empleado`);
+  ADD CONSTRAINT `precios_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`) ON DELETE SET NULL ON UPDATE SET NULL,
+  ADD CONSTRAINT `precios_ibfk_2` FOREIGN KEY (`id_empleado`) REFERENCES `empleado` (`id_empleado`) ON DELETE SET NULL ON UPDATE SET NULL;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
