05bb3dc74a3d36ca31ff25f01fcd8d9e2386ad2d
yzornetta <yzornetta@gmail.com>
2016-05-15 22:26:56 -0300

    Utilizamos password_hash para el cifrado de las password en la base y las comparamos con password_verify

--- PreciosCuidados/filtro.php
+++ PreciosCuidados/filtro.php
@@ -2,7 +2,9 @@
 <?php
 $id = $_POST['id'];
 $contrasenia = $_POST['contrasenia'];
-//$contramd5= md5($contrasenia);
+$opciones = ['cost' => 12,];
+
+//echo password_hash($contrasenia, PASSWORD_DEFAULT, $opciones);
 
 	function conectarse() {
 		if(!($conexion = mysql_connect("localhost", "root", ""))) {
@@ -23,32 +25,34 @@ $contrasenia = $_POST['contrasenia'];
 		//Conexión a la Base de Datos
 		$conexion = conectarse();
 		mysql_select_db("precioscuidados", $conexion); 
-		$query 	= "SELECT * FROM usuario WHERE id = '$id' AND contrasenia = '$contrasenia'"; 
-		//$query 	= "SELECT * FROM usuario WHERE id = '$id' AND contrasenia = '$contramd5'"; 
+		$query 	= "SELECT * FROM usuario WHERE id = '$id'";
 		$consulta 		= mysql_query($query, $conexion); 
 		$cant	 	= mysql_num_rows($consulta); 
 		$fila   = mysql_fetch_array($consulta);
+		$ContraseniaHash=$fila['contrasenia'];
+
 		if ($cant == 0) { //NO ESTA EN LA BASE
-			echo "Error: Usuario o Contraseña incorrectos"; 
-			header("location:login.php"); 
-		                }
+		echo "Error: Usuario incorrecto"; 
+		//header("location:login.php"); 
+		}
 		else {
 
-			if ($fila['rol'] == 'administrador'){
+			if(password_verify($contrasenia, $ContraseniaHash)){
+			//echo '¡La contraseña es válida!';			 
+				if ($fila['rol'] == 'administrador'){
 				session_start();
 				$_SESSION['log']=1;
 				$_SESSION['id'] = $id;
 				header("location: administrador/indexAdministrador.php");
-			} 
-			else
-			{
+				} 
+				else
+				{
 				session_start();
 				$_SESSION['log']=1;
 				$_SESSION['id'] = $id;
 				header("location:usuario/indexUsuario.php");
-			
-			
-		}
-	} 
+				}
+			}else{echo "Error: Contraseña incorrecta"; }
+		} 
 }
 ?>
